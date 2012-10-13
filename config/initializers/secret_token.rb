1e4fe9cb9a6f75cca9fc62be9a966c78f0eb3b3a
Maciej Litwiniuk <maciej@litwiniuk.net>
2012-10-13 06:55:04 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScrappsApp::Application.config.secret_token = 'f312a430c3dc1c588b295917b11f405324414adfb146cd32537c5c3d06340128c38baad13db55ac7ecd64b221aff15889f5bc6c3bf66c90fb3b885fa91dadc40'
